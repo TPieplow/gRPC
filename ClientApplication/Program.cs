dc89afc0427f0c2b567933e59333105d57373896
Ted Pieplow <ted.pieplow@gmail.com>
2024-04-22 15:42:28 +0200

    first commit

---
+++ ClientApplication/Program.cs
@@ -0,0 +1,24 @@
+﻿using ClientApplication;
+using ClientApplication.Services;
+
+var grpc = new GrpcService("https://localhost:7014");
+
+
+// Subscribe
+Console.Clear();
+Console.Write("Email to subscribe: ");
+var subscribeRequest = new SubscribeRequest { Email = Console.ReadLine() };  
+
+var subscribeResponse = await grpc.SubscribeAsync(subscribeRequest);
+Console.WriteLine($"{subscribeResponse.Message}");
+Console.ReadKey();
+
+
+// Unsubscribe
+Console.Clear();
+Console.Write("Email to unsubscribe: ");
+var unsubscribeRequest = new UnsubscribeRequest { Email = Console.ReadLine() };
+
+var unsubscriberesponse = await grpc.UnsubscribeAsync(unsubscribeRequest);
+Console.WriteLine($"{unsubscriberesponse.Message}");
+Console.ReadKey();
---
+++ ClientApplication/Services/GrpcService.cs
@@ -0,0 +1,27 @@
+﻿using Grpc.Net.Client;
+
+namespace ClientApplication.Services;
+
+public class GrpcService
+{
+    private readonly GrpcChannel _channel;
+    private readonly SubscriptionService.SubscriptionServiceClient _client;
+
+    public GrpcService(string serverAddress)
+    {
+        _channel =  GrpcChannel.ForAddress(serverAddress);
+        _client = new SubscriptionService.SubscriptionServiceClient(_channel);
+    }
+
+    public async Task<SubscribeResponse> SubscribeAsync(SubscribeRequest request)
+    {
+        var response = await _client.SubscribeAsync(request);
+        return response;
+    }
+
+    public async Task<UnsubscribeResponse> UnsubscribeAsync(UnsubscribeRequest request)
+    {
+        var repsonse = await _client.UnsubscribeAsync(request);
+        return repsonse;
+    }
+}
